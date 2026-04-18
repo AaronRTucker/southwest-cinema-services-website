c1f3169e355605091444f87eba6ce4833304f8e6
AaronRTucker <artuckeraustin@gmail.com>
2026-04-17 20:51:27 -0500

    Allow artuckeraustin.synology.me as dev origin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.1.240"],
+  allowedDevOrigins: ["192.168.1.240", "artuckeraustin.synology.me"],
 };
 
 export default nextConfig;
